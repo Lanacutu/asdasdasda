d6331d0499b554ebd72fc215f4d549113a2b0098
Lanacutu <ganduchaddi0@gmail.com>
2024-07-28 17:38:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ function popBalloon(message, balloonElement) {
   const popSound = document.getElementById('pop-sound');
   popSound.play(); // Play pop sound
 
+  // Add pop animation
   balloonElement.classList.add('pop-animation');
+  
+  // Hide the balloon after the animation
   setTimeout(() => {
-    balloonElement.style.display = 'none'; // Hide balloon after animation
+    balloonElement.style.display = 'none'; // Hide balloon
     const messageElement = document.getElementById('message');
     messageElement.innerHTML = `<p>${message}</p>`;
     messageElement.classList.add('show'); // Show message
